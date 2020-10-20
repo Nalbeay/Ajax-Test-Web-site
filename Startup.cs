aa8a34fe49e2230be8b06ba0f47d194879d76816
Никита Волосович <qweeree01@gmail.com>
2020-10-20 18:49:30 +0700

    Создал проект для проверки частичного представления.

---
+++ Startup.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+namespace TestSasha
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
---
+++ wwwroot/js/Patrial.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    GetStudents();
+});
+
+
+$('#ClickButton').on('click', function (e) {
+    var stageId = $('#stageId').val();
+    GetStudents(stageId);
+});
+
+function GetStudents(filters) {
+    $.ajax({
+        url: '/Home/Students',
+        type: 'POST',
+        cache: false,
+        async: true,
+        dataType: "html",
+        data: filters
+    })
+        .done(function (result) {
+            $('#students').html(result);
+        });
+}
